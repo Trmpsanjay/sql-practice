46ed4d6a6fb6657b06dc513aad29afc7a9871c3f
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2021-04-04 14:29:55 +0530

    Create cityObservationstation12.sql

---
+++ cityObservationstation12.sql
@@ -0,0 +1,12 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+SELECT DISTINCT CITY FROM STATION  WHERE  LOWER(SUBSTR(CITY,1,1)) NOT IN ('a','e','i','o','u') AND LOWER(SUBSTR(CITY,-1)) NOT IN  ('a','e','i','o','u');
+
+
+-- SELECT DISTINCT CITY FROM STATION WHERE RIGHT (CITY, 1) NOT IN ('A','E', 'I', 'O', 'U') OR LEFT (CITY,1) IN ('A','E', 'I', 'O', 'U');
